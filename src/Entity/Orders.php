912e03deb201742737906a57202362db85351e26
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-05-30 10:39:15 +0200

    DB created

---
+++ src/Entity/Orders.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrdersRepository::class)]
+class Orders
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 20,unique: true)]
+    private $reference;
+
+    #[ORM\Column(type: 'datetime_immutable', options: ['defautl' =>'CURRENT_TIMESTAMP'])]
+    private $created_at;
+
+    #[ORM\ManyToOne(targetEntity: Coupons::class, inversedBy: 'orders')]
+    private $coupons;
+
+    #[ORM\ManyToOne(targetEntity: Users::class, inversedBy: 'orders')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $users;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getCoupons(): ?Coupons
+    {
+        return $this->coupons;
+    }
+
+    public function setCoupons(?Coupons $coupons): self
+    {
+        $this->coupons = $coupons;
+
+        return $this;
+    }
+
+    public function getUsers(): ?Users
+    {
+        return $this->users;
+    }
+
+    public function setUsers(?Users $users): self
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+}
